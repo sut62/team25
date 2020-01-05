542f31799eda4414b631e013899c040fb0781f7d
thanyapong <thanyapong_tr@hotmail.com>
2020-01-05 13:06:07 +0000

    close #58 - ทำการเพิ่ม Entity ของระบบย่อย ระบบสมัครสมาชิกของผู้ประกอบการ

---
+++ backend/src/main/java/com/cpe/backend/entity/CompanyType.java
@@ -0,0 +1,36 @@
+package com.cpe.backend.entity;
+
+import lombok.*;
+
+import javax.persistence.Id;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.FetchType;
+
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name="COMPANYTYPE")
+public class CompanyType {
+	@Id
+	@SequenceGenerator(name="TYPE_SEQ",sequenceName="TYPE_SEQ")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="TYPE_SEQ")
+	@Column(name="TYPE_ID",unique = true, nullable = true)
+	private @NonNull Long id;
+	private @NonNull String name;
+	
+	@OneToMany(fetch = FetchType.EAGER)
+	// mappedBy  = "type"
+	private Collection<Company> register;
+
+}
