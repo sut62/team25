be6e0e780c510fdb94f19aadc5933f3d5561d952
nuttawan1998 <nuttawan.p1998@gmail.com>
2020-01-09 21:24:15 +0700

    close #88 - ทำการแก้ไขหน้า UI ของระบบลงทะเบียนผู้สมัครงาน

--- client/src/components/LoginUser.vue
+++ client/src/components/LoginUser.vue
@@ -1,43 +1,61 @@
 <template>
+<v-card class = "mx-auto" height="100%" width="100%">
+    <v-navigation-drawer 
+    absolute
+    src="https://images.unsplash.com/photo-1516321165247-4aa89a48be28?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1094&q=80"
+    width="100%"
+    parmanent>
        <v-container>
    <br>
    <br>
    <br>
 <v-card 
-  max-width="700"
+  max-width="800"
   class="mx-auto"
+  color=#EEEEEE
+  :elevation="20"
 >
-<v-system-bar color="green"></v-system-bar>
-<v-system-bar color="green"></v-system-bar>
+<v-system-bar color="#393939"></v-system-bar>
+<v-system-bar color="#424242"></v-system-bar><br>
+<v-card class="mx-auto" max-width="180" color="#424242" :elevation="15" dark>
 <v-row justify="center">
-   <v-toolbar-title ><h1>Login</h1></v-toolbar-title>
+   <v-toolbar-title><h1>Login</h1></v-toolbar-title>
 </v-row>
+</v-card><br>
     <v-row justify="center">
-            <v-col cols="5">
+            <v-col cols="6">
               <v-text-field
+              color="#000000"
                 solo
                 label="Email"
                 v-model= "email"
                 :rules="[(v) => !!v || 'This field is required']"
                 required
+                class="font-weight-bold"
                 clearable
+                rounded
+                outlined
                 prepend-icon="mdi-email"
               ></v-text-field>
             </v-col>
           </v-row>
  
        <v-row justify="center">
-            <v-col cols="5">
+            <v-col cols="6">
               <v-text-field
                 solo
+                color="#000000"
                 label="PASSWORD"
                 v-model= "password"
                 :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
                 :type="show ? 'text' : 'password'"
                 prepend-icon="mdi-lock"
                 required
+                class="font-weight-bold"
                 counter
                 clearable
+                outlined
+                rounded
                 @click:append="show = !show"
               ></v-text-field>
             </v-col>
@@ -45,25 +63,25 @@
 
            <v-row justify="center">
             <v-col >
-              <v-btn 
+              <v-btn rounded loaders color="#000000" :elevation="5" dark
                 style="margin-left:25%;"
-                @click="BLogin">Login
+                @click="BLogin" class="font-weight-black"><v-icon dark left>mdi-login</v-icon>Login
               </v-btn>
-             
-            <v-btn 
+    
+              <v-btn rounded loaders color="#000000" :elevation="5" dark
                 style="margin-left:25%;"
-                @click="Register">Register
-                 
+                @click="Register" class="font-weight-black"><v-icon dark left>mdi-account-plus</v-icon>Register
               </v-btn>
-     
             </v-col>
           </v-row>
 <br>
 <br>
-<v-system-bar color="green"></v-system-bar>
-<v-system-bar color="green"></v-system-bar>
+<v-system-bar color="#424242"></v-system-bar>
+<v-system-bar color="#393939"></v-system-bar>
   </v-card>
    </v-container>
+   </v-navigation-drawer>
+  </v-card>
 </template>
 
 
