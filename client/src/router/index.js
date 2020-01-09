9ad40b5c4778f0d10ad48e838d33fbd68643fbae
xxnnooaa <nanticha.ntc1608@gmail.com>
2020-01-09 23:01:11 +0700

    close #94 - แก้ไขไฟล์ index.js ของระบบสมัครงาน

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,26 +1,63 @@
-import Vue from 'vue'
-import Router from 'vue-router';
-
-import RegisterCompany from '../components/RegisterCompany';
-import LoginCompany from '../components/LoginCompany';
-import Companychoice from '../components/Companychoice';
+import Vue from "vue";
+import Router from "vue-router";
+import Addjob from "../components/Addjob";
+import Home from "../components/Home";
+import Registeruser from "../components/Registeruser";
+import LoginUser from "../components/LoginUser";
+import Userchoice from "../components/Userchoice";
+import LoginCompany from "../components/LoginCompany";
+import RegisterCompany from "../components/RegisterCompany";
+import Companychoice from "../components/Companychoice";
+import JobPost from "../components/JobPost";
 
 Vue.use(Router);
 
 export default new Router({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes: [{
-            path: '/',
-            component: LoginCompany
-        },
-        {
-            path: '/RegisterCompany',
-            component: RegisterCompany
-        },
-        {
-            path: '/Companychoice',
-            component: Companychoice
-        },
-    ]
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    
+    {
+      path: "/home",
+      component: Home
+    },
+    {
+      path: "/addjob",
+      component: Addjob
+    },
+    {
+        path: '/loginuser',
+        component: LoginUser
+    },
+    {
+      path: "/registeruser",
+      component: Registeruser
+    },
+    {
+        path: "/userchoice",
+        component: Userchoice
+    },
+    {
+      path: "/logincompany",
+      component: LoginCompany
+    },
+    {
+      path: "/registercompany",
+      component: RegisterCompany 
+    },
+    {
+      path: "/companychoice",
+      component: Companychoice 
+    },
+    {
+      path: "/jobpost",
+      component: JobPost 
+    }
+
+  ]
 });
